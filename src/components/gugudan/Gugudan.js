ecbac26719ee9c1f4469b8e75dd580fbdc0791fb
DeokHaengCho <stargazer61@naver.com>
2020-11-04 18:58:33 +0900

    2020.11.04

--- src/components/gugudan/Gugudan.js
+++ src/components/gugudan/Gugudan.js
@@ -2,32 +2,31 @@ import React, {useState, useEffect} from 'react'
 
 function Gugudan() {
     const [number, setNumber] = useState({
-        q1 : 0,
-        q2: 0
+        first : Math.ceil(Math.random()*9),
+        second : Math.ceil(Math.random()*9)
     })
-    const [answerNumber, setAnswerNumber] = useState(0)
+    const [answerNumber, setAnswerNumber] = useState('')
     const [complete, setComplete]= useState(false)
-    const [submit, setSubmit] = useState(false)
-    useEffect(()=>{
-        let q1Number = Math.floor(Math.random()*10)
-        let q2Number = Math.floor(Math.random()*10)
-        setNumber({q1:q1Number, q2:q2Number})
-    },[])
 
-    useEffect(()=>{
-        if(submit&&number.q1*number.q2==answerNumber){
+    const onChange = e => {
+        setAnswerNumber(e.currentTarget.value)
+    }
+
+    const onSubmit = e => {
+        if(parseInt(answerNumber)===number.first*number.second){
             setComplete(true)
         }else{
-            setSubmit(false)
             setComplete(false)
         }
-    },[answerNumber, submit])
+    }
 
     return (
         <div>
-            <div>{number.q1} 곱하기 {number.q2} 은?</div>
-            <input type="number" value={answerNumber} onChange={e=>{setAnswerNumber(e.currentTarget.value)}}></input>
-            <button onClick={()=>setSubmit(true)}>제출</button>
+            <div>{number.first} 곱하기 {number.second} 은?</div>
+                <input type="number" value={answerNumber} 
+                    onChange={onChange}/>
+            <button onClick={onSubmit}>제출</button>
+            
             <div>{complete? "정답": "땡!"}</div>
         </div>
     )
